3fab5139b789a1f017414b4d19ff6109f3d5dfc7
Saiful I. Wicaksana <icaksama@gmail.com>
2025-06-04 16:45:35 +0700

    add: api endpoints url

---
+++ lib/core/constants/api_endpoints.dart
@@ -0,0 +1,13 @@
+class ApiEndpoints {
+  static const String baseUrl = 'http://10.0.2.2:8000';
+
+  // Auth endpoints
+  static const String register = '$baseUrl/auth/register';
+  static const String login = '$baseUrl/auth/login';
+
+  // Posts endpoints
+  static const String postCreate = '$baseUrl/posts/create';
+  static const String postUpdate = '$baseUrl/posts/update';
+  static const String postDelete = '$baseUrl/posts/delete';
+  static const String postList = '$baseUrl/posts/list';
+}
